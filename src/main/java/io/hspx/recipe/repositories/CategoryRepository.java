b527efb326b137883c2273591c02887b319b004a
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-04-06 20:35:30 +0530

    Initial Code base

---
+++ src/main/java/io/hspx/recipe/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package io.hspx.recipe.repositories;
+
+import io.hspx.recipe.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
+}
